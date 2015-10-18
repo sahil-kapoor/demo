fd59d47d203d9963f83b9e5519cd3394d00555a1
anandvihar <sahilkapoor1987@gmail.com>
2015-10-18 17:56:09 +0530

    Base 0.1

---
+++ demoBase/src/main/java/com/base/service/UserService.java
@@ -0,0 +1,27 @@
+package com.base.service;
+
+import java.util.List;
+
+import com.base.model.User;
+
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
