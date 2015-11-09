633d0b9c5cabc7d5f36e1cba724f7714d51a5728
Sahil Kapoor <sahil.kapoor@markitserv.com>
2015-11-09 20:11:07 +0530

    Logging integrated for wildfly

--- demoBase/src/main/java/com/base/dao/city/CityDaoImpl.java
+++ demoBase/src/main/java/com/base/dao/city/CityDaoImpl.java
@@ -1,12 +1,16 @@
 package com.base.dao.city;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.base.dao.AbstractDao;
 
 public class CityDaoImpl implements CityDao {
 
 	
 	private AbstractDao abstractDao; 
-
+	private static final Logger logger = LoggerFactory.getLogger(CityDaoImpl.class);;
+	 
 	public CityDaoImpl(AbstractDao abstractDao) {
 		this.abstractDao=abstractDao;
 	}
@@ -14,6 +18,7 @@ public class CityDaoImpl implements CityDao {
 	@Override
 	public String getNameById(String Id) {
 		String query="Select name from city where ID=?";
+		logger.debug(query);
 		String name= (String)abstractDao.getJdbcTemplate().queryForObject(
 				query, new Object[] { Id }, String.class);
 		
