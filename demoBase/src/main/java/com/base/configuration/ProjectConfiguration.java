25f9220ec48b255d0b16c8aeba35b973128ad5df
Sahil Kapoor <sahil.kapoor@markitserv.com>
2015-11-12 18:25:53 +0530

    Integrated swagger

--- demoBase/src/main/java/com/base/configuration/ProjectConfiguration.java
+++ demoBase/src/main/java/com/base/configuration/ProjectConfiguration.java
@@ -5,8 +5,11 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.base.dao.AbstractDao;
 import com.base.dao.city.CityDao;
@@ -15,8 +18,9 @@ import com.base.dao.city.CityDaoImpl;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.base")
-public class ProjectConfiguration {
-	
+@Import(SwaggerConfiguration.class)
+public class ProjectConfiguration extends WebMvcConfigurerAdapter {
+
 	@Bean
 	public DataSource getDataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
@@ -24,17 +28,24 @@ public class ProjectConfiguration {
 		dataSource.setUrl("jdbc:mysql://localhost:3306/world");
 		dataSource.setUsername("root");
 		dataSource.setPassword("root");
-		
+
 		return dataSource;
 	}
-	
-	
+
 	@Bean
 	public AbstractDao getAbstractDao() {
 		return new AbstractDao(getDataSource());
 	}
+
 	@Bean
 	public CityDao fooService() {
-        return new CityDaoImpl(getAbstractDao());
-    }
+		return new CityDaoImpl(getAbstractDao());
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 }
